b131c88161be76177dbcb9845017b878118f6b9b
sakir tufan <sakirtufan49@gmail.com>
2021-03-08 21:45:16 +0100

    Add Categories Dynamically To Admin Page

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../../models/Category")
 
 
 router.get("/", (req, res) => {
@@ -7,7 +8,17 @@ router.get("/", (req, res) => {
 });
 
 router.get("/categories", (req, res) => {
-  res.render("admin/categories");
+  Category.find({}).then(categories => {
+    res.render("admin/categories", { categories: categories})
+  })  
+});
+
+router.post("/categories", (req, res) => {
+  Category.create(req.body, (error,category) => {
+    if(!error){
+      res.redirect('categories')
+    }
+  })
 });
 
 
