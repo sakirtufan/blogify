e48d37ae1ccf45738d6b7cacaa9258288c063ef0
sakir tufan <sakirtufan49@gmail.com>
2021-03-10 16:12:25 +0100

    Edit Post Count And Links For Categories

--- routes/posts.js
+++ routes/posts.js
@@ -6,67 +6,95 @@ const Category = require("../models/Category");
 const User = require("../models/User");
 
 router.get("/new", (req, res) => {
-  if(!req.session.userId) {
-    res.redirect('/users/login')
-  }
+   if (!req.session.userId) {
+      res.redirect("/users/login");
+   }
 
-  Category.find({}).sort({$natural:-1}).then((categories) => {
-    res.render("site/addPost", { categories: categories})
-  })
-  
+   Category.find({})
+      .sort({ $natural: -1 })
+      .then((categories) => {
+         res.render("site/addPost", { categories: categories });
+      });
+});
+
+router.get("/category/:categoryId", (req, res) => {
+   Post.find({ category: req.params.categoryId })
+      .populate({ path: "category", model: Category })
+      .then((posts) => {
+        Category.aggregate([
+            {
+               $lookup: {
+                  from: "posts",
+                  localField: "_id",
+                  foreignField: "category",
+                  as: "posts",
+               },
+            },
+            {
+               $project: {
+                  _id: 1,
+                  name: 1,
+                  num_of_posts: { $size: "$posts" },
+               },
+            },
+         ]).then((categories) => {
+           res.render('site/blog',{posts: posts, categories: categories});
+         })
+      });
 });
 
 router.get("/:id", (req, res) => {
-  Post.findById(req.params.id).populate({path:'author', model: User}).then((post) => {
-    Category.aggregate([
-       {
-          $lookup: {
-             from: "posts",
-             localField: "_id",
-             foreignField: "category",
-             as: "posts",
-          },
-       },
-       {
-          $project: {
-             _id: 1,
-             name: 1,
-             num_of_posts: { $size: "$posts" },
-          },
-       },
-    ]).then((categories) => {
-          Post.find({})
-             .populate({ path: "author", model: User })
-             .sort({ $natural: -1 })
-             .then((posts) => {
-                res.render("site/post", {
-                   post: post,
-                   categories: categories,
-                   posts: posts,
-                });
-             });
-       });
-  });
+   Post.findById(req.params.id)
+      .populate({ path: "author", model: User })
+      .then((post) => {
+         Category.aggregate([
+            {
+               $lookup: {
+                  from: "posts",
+                  localField: "_id",
+                  foreignField: "category",
+                  as: "posts",
+               },
+            },
+            {
+               $project: {
+                  _id: 1,
+                  name: 1,
+                  num_of_posts: { $size: "$posts" },
+               },
+            },
+         ]).then((categories) => {
+            Post.find({})
+               .populate({ path: "author", model: User })
+               .sort({ $natural: -1 })
+               .then((posts) => {
+                  res.render("site/post", {
+                     post: post,
+                     categories: categories,
+                     posts: posts,
+                  });
+               });
+         });
+      });
 });
 
 router.post("/test", (req, res) => {
-  let post_image = req.files.post_image;
-  post_image.mv(
-    path.resolve(__dirname, "../public/img/postimages", post_image.name)
-  );
-  Post.create({
-    ...req.body,
-    post_image: `/img/postimages/${post_image.name}`,
-    author: req.session.userId,
-  }, );
+   let post_image = req.files.post_image;
+   post_image.mv(
+      path.resolve(__dirname, "../public/img/postimages", post_image.name)
+   );
+   Post.create({
+      ...req.body,
+      post_image: `/img/postimages/${post_image.name}`,
+      author: req.session.userId,
+   });
 
-  req.session.sessionFlash = {
-    type:'alert alert-success',
-    message: 'Your post has been created successfully'
-  }
+   req.session.sessionFlash = {
+      type: "alert alert-success",
+      message: "Your post has been created successfully",
+   };
 
-  res.redirect("/blog");
+   res.redirect("/blog");
 });
-
 
 module.exports = router;
